181e1530abfa9b9cbe43ce3b10baed0d3c1916cb
Brandon Parker <bparkerproductions@gmail.com>
2021-07-21 13:19:45 -0700

    resume layout start

--- resources/views/template-resume.blade.php
+++ resources/views/template-resume.blade.php
@@ -8,12 +8,18 @@
   @component('components.header', [
     'icon_class' => 'fas fa-file-code'
   ])
-    <p>Resume</p>
   @endcomponent
 
   <section class="resume demos spacer-small column-center">
-    <div class="inner-container demos-section">
-      <p>Resume</p>
+    <div class="inner-container">
+      <div class="resume-content card">
+        <div class="resume-main-content">
+          Col 1
+        </div>
+        <aside>
+          Col 2
+        </aside>
+      </div>
     </div>
   </section>
 @endsection
