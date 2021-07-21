715521b6c04823f4c550f59d384eb2e087379b43
Brandon Parker <bparkerproductions@gmail.com>
2021-07-21 13:57:26 -0700

    resume intro section

--- resources/views/template-resume.blade.php
+++ resources/views/template-resume.blade.php
@@ -12,12 +12,21 @@
 
   <section class="resume demos spacer-small column-center">
     <div class="inner-container">
-      <div class="resume-content card">
+      <div class="resume-content card no-hover">
         <div class="resume-main-content">
           <h2 class="resume-title">Brandon Parker</h2>
 
           <div class="resume-intro">
-            <p>{{$resume['intro']}}</p>
+            <p class="m-0">{{$resume['intro']}}</p>
+
+            <ul class="resume-intro-list">
+              @foreach($resume['intro_points'] as $point)
+                <li>
+                  <i class="fas fa-plus-circle resume-intro-icon"></i>
+                  <p class="m-0">{{$point['point']}}</p>
+                </li>
+              @endforeach
+            </ul>
           </div>
         </div>
         <aside>
