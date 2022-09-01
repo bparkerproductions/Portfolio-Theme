ca7e514c730bc426628fb642c106d6fad7c16ec4
Brandon Parker <bparkerproductions@gmail.com>
2022-09-01 10:14:27 -0700

    add linked in

--- resources/views/partials/global/cta.blade.php
+++ resources/views/partials/global/cta.blade.php
@@ -5,8 +5,13 @@
 
       <ul class="link-container">
         <li>
-          <a href="mailto:bparkerproductions@gmail.com" class="white">
+          <a href="mailto:bparkerproductions@gmail.com" title="bparkerproductions@gmail.com" class="white">
             <i class="fas fa-envelope"></i>Email Me
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/bparkerproductions/" class="white">
+            Linked In
           </a>
         </li>
         <li>
