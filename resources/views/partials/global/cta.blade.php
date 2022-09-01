04471c733c92d3fcca22d10c1a916599b09c7973
Brandon Parker <bparkerproductions@gmail.com>
2022-09-01 10:13:01 -0700

    add mailto link and redesign cta links

--- resources/views/partials/global/cta.blade.php
+++ resources/views/partials/global/cta.blade.php
@@ -3,11 +3,18 @@
     <div class="header-container">
       <h2 class="white">{{$cta_header}}</h2>
 
-      <div class="link-container">
-        <a href="https://www.upwork.com/freelancers/~01069214b17875a221" class="white">
-          Reach me on Upwork
-        </a>
-      </div>
+      <ul class="link-container">
+        <li>
+          <a href="mailto:bparkerproductions@gmail.com" class="white">
+            <i class="fas fa-envelope"></i>Email Me
+          </a>
+        </li>
+        <li>
+          <a href="https://www.upwork.com/freelancers/~01069214b17875a221" class="white">
+            Reach me on Upwork
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </section>
