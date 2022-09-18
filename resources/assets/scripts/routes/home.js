f72da8c4b996720204572a5bbe6745a59c5aab27
Brandon Parker <bparkerproductions@gmail.com>
2022-09-17 18:56:10 -0700

    refactor home.js to vanilla js

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -2,14 +2,11 @@ import Scroll from '../components/scroll'
 
 export default {
   init() {
-    this.setEventListeners()
+    document.querySelectorAll('.scroll-down-home')[0].addEventListener(
+      'click',
+      this.scrollDown.bind(this)
+    )
   },
-  setEventListeners() {
-    $('.scroll-down-home').on('click', Home.scrollDown);
-  },
-}
-
-let Home = {
   scrollDown(e) {
     e.preventDefault();
     Scroll.to('.home-about')
