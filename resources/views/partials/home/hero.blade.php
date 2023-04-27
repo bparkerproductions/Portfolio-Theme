7352fb448152aae7685be8dc9330ad6d24f7104a
Brandon Parker <bparkerproductions@gmail.com>
2023-04-27 09:08:19 -0700

    bs primary and secondary buttons

--- resources/views/partials/home/hero.blade.php
+++ resources/views/partials/home/hero.blade.php
@@ -1,18 +1,18 @@
 <section class="hero bubble-header d-flex">
   <div class="container d-flex align-items-center justify-content-start">
     <div class="row">
-      <h1 class="mb-5 text-primary">Explore. Learn. Create.</h1>
+      <h1 class="mb-5">Explore. Learn. Create.</h1>
 
       <div class="row">
         <div class="col-4 me-3">
           <div class="card card-body d-flex flex-column justify-content-between">
             <div>
-              <h4 class="text-primary">Book.</h4>
+              <h4 class="">Book.</h4>
               <p>I am currently open for work. Book a session today</p>
             </div>
 
             <div class="mt-3">
-              <button class="btn btn-secondary btn-lg">Book a Service</button>
+              <button class="btn btn-primary btn-lg">Book a Service</button>
             </div>
           </div>
         </div>
@@ -20,7 +20,7 @@
         <div class="col-4">
           <div class="card card-body d-flex flex-column justify-content-between">
             <div>
-              <h4 class="text-primary">Explore</h4>
+              <h4 class="">Explore</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit praesentium numquam illum expedita.</p>
             </div>
 
