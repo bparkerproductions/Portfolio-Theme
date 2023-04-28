e7fc2d50bab7c268cae72224ce1f07f18bdaacbd
Brandon Parker <bparkerproductions@gmail.com>
2023-04-28 09:45:41 -0700

    make home hero mobile responsive

--- resources/views/partials/home/hero.blade.php
+++ resources/views/partials/home/hero.blade.php
@@ -4,7 +4,7 @@
       <h1 class="mb-5">Explore. Learn. Create.</h1>
 
       <div class="row">
-        <div class="col-4 me-3">
+        <div class="col-12 col-md-6 col-xl-4 me-0 me-xl-3 mb-4 mb-md-0">
           <div class="card card-body d-flex flex-column justify-content-between">
             <div>
               <h4 class="">Book.</h4>
@@ -17,7 +17,7 @@
           </div>
         </div>
 
-        <div class="col-4">
+        <div class="col-12 col-md-6 col-xl-4">
           <div class="card card-body d-flex flex-column justify-content-between">
             <div>
               <h4 class="">Explore</h4>
