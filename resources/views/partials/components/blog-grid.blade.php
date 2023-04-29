79293210b2824061ffced9d999a45289ba8085da
Brandon Parker <bparkerproductions@gmail.com>
2023-04-29 14:00:44 -0700

    blog preview title unification

--- resources/views/partials/components/blog-grid.blade.php
+++ resources/views/partials/components/blog-grid.blade.php
@@ -4,11 +4,11 @@
   <ul class="row list-unstyled">
     @foreach($blog_list as $ID)
       <li class="blog-preview col-12 col-md-6 col-xl-4 my-4">
-        <article class="position-relative">
+        <article title="{{get_the_title($ID)}}" class="position-relative">
           <a class="absolute-fill" href="{{get_the_permalink($ID)}}"></a>
 
           <img class="img-fluid shadow rounded mb-4" src="{{ get_the_post_thumbnail_url($ID) }}" />
-          <h4 title="get_the_title($ID)" class="single-blog-title">{!! get_the_title($ID) !!}</h4>
+          <h4 title="{{get_the_title($ID)}}" class="blog-preview__title">{!! get_the_title($ID) !!}</h4>
           <p class="single-blog-description">{!! get_the_excerpt($ID) !!}</p>
 
           <p class="text-primary mb-0">Read More</p>
