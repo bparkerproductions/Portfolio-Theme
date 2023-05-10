df784073a0c55cc1dd597d74fa0ea3cff8acdf6c
Brandon Parker <bparkerproductions@gmail.com>
2023-05-10 11:14:27 -0700

    client project layout progress

--- resources/assets/js/pages/project.js
+++ resources/assets/js/pages/project.js
@@ -2,15 +2,26 @@
   if ( !document.getElementById('all-projects')) return
 
   // document.querySelectorAll('.project__gallery').forEach( elem => {
-    new window.Glider(document.querySelector('.project__gallery__container'), {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      draggable: true,
-      scrollLock: true,
-      arrows: {
-        next: '.next-arrow',
-        prev: '.prev-arrow'
-      }
-    })
+    // new window.Glider(document.querySelector('.project__gallery__container'), {
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1,
+    //   draggable: true,
+    //   scrollLock: true,
+    //   arrows: {
+    //     next: '.next-arrow',
+    //     prev: '.prev-arrow'
+    //   }
+    // })
   // })
+
+  document.querySelectorAll('.all-projects .project').forEach( elem => {
+    const leftContainer = elem.querySelector('.project__left--container')
+    const rightContainer = elem.querySelector('.project__right--container')
+    const rightContainerHeader = elem.querySelector('.project__right-container__header')
+    const projectContent = elem.querySelector('.project__content')
+
+    projectContent.style.height = leftContainer.clientWidth + "px"
+    // projectContent.style.height = rightContainer.clientWidth - rightContainerHeader.clientWidth + "px"
+    
+  })
 })()
--- resources/views/template-projects.blade.php
+++ resources/views/template-projects.blade.php
@@ -6,33 +6,34 @@
 
 @section('content')
   <section id="all-projects" class="no-header-padding">
-    <div class="container">
-
       @foreach(get_field('all_projects') as $id)
-        <article class="project py-5">
-          <div class="d-flex align-items-center mb-2">
-            @if(get_field('client_projects', $id)['is_featured'])
-              <p class="badge bg-secondary mb-0 me-3">Featured</p>
-            @endif
-            <div class="icon-container">
-              @foreach(get_field('client_projects', $id)['tech'] as $techID)
-                @if (get_field('fa_icon_class', $techID))
-                  <i class="{{get_field('fa_icon_class', $techID)}} fa-lg me-2"></i>
-                @endif
-              @endforeach
+        <article 
+          style="background-color: {{get_field('client_projects', $id)['color']}};" 
+          class="project py-5 @if(get_field('client_projects', $id)['light']) light @endif"
+        >
+          <div class="container">
+            <div class="d-flex align-items-center mb-2">
+              @if(get_field('client_projects', $id)['is_featured'])
+                <p class="badge bg-secondary mb-0 me-3">Featured</p>
+              @endif
+              <div class="icon-container">
+                @foreach(get_field('client_projects', $id)['tech'] as $techID)
+                  @if (get_field('fa_icon_class', $techID))
+                    <i class="{{get_field('fa_icon_class', $techID)}} project__icon fa-lg me-2"></i>
+                  @endif
+                @endforeach
+              </div>
+              
             </div>
-            
-          </div>
-        
-          <h1 class="mb-0" title="{{get_the_title($id)}}">{{get_the_title($id)}}</h1>
+          
+            <h1 class="mb-0 project__title" title="{{get_the_title($id)}}">{{get_the_title($id)}}</h1>
 
-          <div class="row">
+            <div class="row">
 
-            {{-- Image --}}
-            <div class="col-6">
+              {{-- Image --}}
+              <div class="col-6">
 
-                {{-- <div class="row"> --}}
-                {{-- <div class="project__gallery__container row"> --}}
+                <div class="project__left--container">
                   <div class="card h-auto mt-3 py-4">
                     <div class="card-body row">
                       <div class="col-6">
@@ -47,79 +48,75 @@
                       </div>
 
                     </div>
-
-
-                    {{-- @if (get_field('client_projects', $id)['featured_image_description'])
-                      <p class="mb-0 mt-3 text-black-50 mt-2 fs-small">{{get_field('client_projects', $id)['featured_image_description']}}</p>
-                    @endif --}}
                   </div>
 
-                  <div class="position-relative project__gallery">
-                    <div class="prev-arrow project__gallery__arrow fa-2x text-primary">
+                  {{-- <div class="position-relative project__gallery"> --}}
+                    {{-- <div class="prev-arrow project__gallery__arrow fa-2x text-primary">
                       <i class="fas fa-angle-left"></i>
-                    </div>
-                    <div class="project__gallery__container py-4 mx-5 row">
+                    </div> --}}
+                    <div class="project__gallery__container py-4 row">
                       @if (get_field('client_projects', $id)['gallery'])
                         @foreach (get_field('client_projects', $id)['gallery'] as $img)
-                          <div class="project__gallery__slide {{$img['col_width']}}">
-                            <div class="d-flex justify-content-center my-3">
-                              <img class="rounded shadow project__gallery__img" src="{{$img['image']['url']}}" />
-                              {{-- <p class="mb-0 mt-3 text-black-50 mt-2 fs-small">{{$img['image_description']}}</p> --}}
-                            </div>
+                          <div class="{{$img['col_width']}} position-relative">
+                            <img class="rounded shadow project__gallery__img w-100" src="{{$img['image']['url']}}" />
+                            <p class="mb-0 mt-3 text-black-50 mt-2 fs-small project__gallery__image">{{$img['image_description']}}</p>
                           </div>
                         @endforeach
                       @endif
                     </div>
-                    <div class="next-arrow project__gallery__arrow fa-2x text-primary">
+                    {{-- <div class="next-arrow project__gallery__arrow fa-2x text-primary">
                       <i class="fas fa-angle-right"></i>
+                    </div> --}}
+                  {{-- </div> --}}
+
+                      
+                    {{-- </div> --}}
+                  {{-- </div> --}}
+                  
+                  {{-- <div class="project__img-container position-relative">
+                    <img class="shadow rounded w-100 external-link" src="{{get_the_post_thumbnail_url($id)}}" />
+                    <a target="_blank" class="project__external-link" href="{{get_field('client_projects', $id)['project_link']['url']}}">
+                      <i class="fas fa-external-link-alt"></i>
+                      <span class="link-text">
+                        {{get_field('client_projects', $id)['project_link']['title']}}
+                      </span>
+                    </a>
+                  </div> --}}
+                </div>
+              </div>
+
+              {{-- Content --}}
+              <div class="col-6 py-4 ps-5">
+                <div class="project__right--container">
+                  <div class="project__right-container__header">
+                    <div class="project__goals">
+                      <p class="fw-bold mb-0 h5 project__goals__header">Goals</p>
+                      <ul class="list-styled ms-4">
+                        @foreach (get_field('client_projects', $id)['goals'] as $goal)
+                          <li class="my-1">{{$goal['goal']}}</li>
+                        @endforeach
+                      </ul>
                     </div>
                   </div>
+                
+                  {{-- Toggle --}}
+                  {{-- <button class="btn text-dark fw-normal d-flex align-items-center">
+                    See More
+                    <i class="fas fa-angle-down ms-2"></i>
+                  </button> --}}
 
-                  
-                {{-- </div> --}}
-              {{-- </div> --}}
-              
-              {{-- <div class="project__img-container position-relative">
-                <img class="shadow rounded w-100 external-link" src="{{get_the_post_thumbnail_url($id)}}" />
-                <a target="_blank" class="project__external-link" href="{{get_field('client_projects', $id)['project_link']['url']}}">
-                  <i class="fas fa-external-link-alt"></i>
-                  <span class="link-text">
-                    {{get_field('client_projects', $id)['project_link']['title']}}
-                  </span>
-                </a>
-              </div> --}}
-            </div>
+                  <section class="mt-4 project__content">
+                    @php echo get_post_field('post_content', $id) @endphp
+                  </section>
 
-            {{-- Content --}}
-            <div class="col-6 py-4 ps-5">
-              <div class="list-container">
-                <p class="fw-bold mb-0 h5 text-dark">Goals</p>
-                <ul class="list-styled ms-4">
-                  @foreach (get_field('client_projects', $id)['goals'] as $goal)
-                    <li class="my-1">{{$goal['goal']}}</li>
-                  @endforeach
-                </ul>
+                  {{-- <p class="project__see-more">See More</p> --}}
+                </div>
+
               </div>
-             
-
-              
-
-              
-
-              {{-- Toggle --}}
-              {{-- <button class="btn text-dark fw-normal d-flex align-items-center">
-                See More
-                <i class="fas fa-angle-down ms-2"></i>
-              </button> --}}
-
-              <section class="mt-4 pt-4 border-top d-none">
-                @php echo get_post_field('post_content', $id) @endphp
-              </section>
             </div>
           </div>
         </article> 
       @endforeach
-    </div>
   </section>
 
 @endsection
