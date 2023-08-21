1c7c7842fa35bff56fa3a270e7388488fb44b665
Brandon Parker <bparkerproductions@gmail.com>
2023-08-21 11:03:57 -0700

    fix some spacing issues

--- resources/views/template-projects.blade.php
+++ resources/views/template-projects.blade.php
@@ -30,43 +30,53 @@
               <h1 class="mb-0 project__title" title="{{get_the_title($id)}}">{{get_the_title($id)}}</h1>
             </header>
 
-          
-
-            <div class="row">
+            <div class="row px-3">
               <div class="col-4 gx-0">
                 <img class="rounded-0 project__preview-image" src="{{get_the_post_thumbnail_url($id)}}" />
               </div>
-              <div class="col-8 bg-white p-3 pe-5">
+              <div class="col-8 rounded-end bg-white">
                 <header>
-                  <div class="d-flex align-items-center mb-1">
-                    <p class="project__duration text-dark-50 fw-normal fs-small mb-0">{{get_field('client_projects', $id)['duration']}}</p>
+                  <div class="d-flex align-items-center mb-1 p-3 border-bottom bg-gray-100">
+                    <p class="project__duration text-dark-50 fw-normal fs-small mb-0">
+                      {{get_field('client_projects', $id)['duration']}}
+                    </p>
 
                     @if (get_field('client_projects', $id)['filter_tags'])
                       @foreach (get_field('client_projects', $id)['filter_tags'] as $tag)
                         <span class="badge bg-primary ms-2">{{$tag['tag']}}</span>
                       @endforeach
                     @endif
-                  </div>
-              
-                  <p class="mb-2 project__description">{{get_the_excerpt($id)}}</p>
 
-                  <div class="project__goals">
-                    <ul class="list-styled ms-4">
-                      @foreach (get_field('client_projects', $id)['goals'] as $goal)
-                        <li class="my-1 text-dark-75">{{$goal['goal']}}</li>
-                      @endforeach
-                    </ul>
-                  
+                  </div>
+
+                  <div class="p-3">
+                    <h6 class="fw-bold mb-1 text-dark-75">About Them</h6>
+                    <p class="mb-2 project__description">{{get_the_excerpt($id)}}</p>
+                  </div>
+
+                  <div class="p-3 border-top">
+                    @if (get_field('client_projects', $id)['what_i_did'])
+                        <h6 class="fw-bold mb-1">What I Did</h6>
+                        <p class="mb-2 project__description">{{get_field('client_projects', $id)['what_i_did']}}</p>
+                    @endif
+
+                    <div class="project__goals">
+                      <ul class="list-styled ms-4">
+                        @foreach (get_field('client_projects', $id)['goals'] as $goal)
+                          <li class="my-1 text-dark-75">{{$goal['goal']}}</li>
+                        @endforeach
+                      </ul>
+                    </div>
                 </div>
                 </header>
               </div>
 
             </div>
 
-            <div class="project__gallery__container row mt-3">
+            <div class="project__gallery__container row mt-3 px-1">
               @if (get_field('client_projects', $id)['gallery'])
                 @foreach (get_field('client_projects', $id)['gallery'] as $img)
-                  <div class="col-6 position-relative">
+                  <div class="{{$img['col_width']}} position-relative">
                     <img class="project__gallery__img rounded shadow w-100" src="{{$img['image']['url']}}" />
                     <p class="mb-0 mt-3 text-dark-50 mt-2 fs-small project__gallery__image">{{$img['image_description']}}</p>
                   </div>
