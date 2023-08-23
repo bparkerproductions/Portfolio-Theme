310f1e5a6181c5fd7a547c6be5fa0b5bd94ed8dd
Brandon Parker <bparkerproductions@gmail.com>
2023-08-23 15:05:55 -0700

    template-projects slight cleanup

--- resources/views/template-projects.blade.php
+++ resources/views/template-projects.blade.php
@@ -7,20 +7,21 @@
 @section('content')
   <section id="all-projects" class="header-transparent-dark bg-gray-400">
       @foreach(get_field('all_projects') as $id)
-        <article 
+        @php $clientProjects = get_field('client_projects', $id); @endphp
+        <article
           class="project py-5 d-flex align-items-center"
-          data-bg-color="{{get_field('client_projects', $id)['color']}}"
+          data-bg-color="{{$clientProjects['color']}}"
         >
           <div class="container">
 
             {{-- Header Information --}}
             <header class="mb-3">
               <div class="d-flex align-items-center mb-1">
-                @if(get_field('client_projects', $id)['is_featured'])
+                @if($clientProjects['is_featured'])
                   <p class="badge bg-secondary mb-0 me-3 py-2 px-3 rounded-0">Featured</p>
                 @endif
                 <div class="icon-container">
-                  @foreach(get_field('client_projects', $id)['tech'] as $techID)
+                  @foreach($clientProjects['tech'] as $techID)
                     @if (get_field('fa_icon_class', $techID))
                       <i class="{{get_field('fa_icon_class', $techID)}} project__icon fa-lg me-2"></i>
                     @endif
@@ -35,23 +36,22 @@
               <div class="col-4 position-relative gx-0 project__preview-image">
                 <div
                   class="absolute-fill project__preview-image--overlay"
-                  style="background-color: {{get_field('client_projects', $id)['color']}}"></div>
+                  style="background-color: {{$clientProjects['color']}}"></div>
                 <img
                   class="rounded-0"
                   src="{{get_the_post_thumbnail_url($id)}}"
                   alt="The logo for the {{get_the_title($id)}} website."
                 />
-                {{-- <h1 class="mb-0 text-white">Fixd Repair</h1> --}}
               </div>
               <div class="col-8 rounded-end bg-white">
                 <header>
                   <div class="d-flex align-items-center mb-1 p-3 border-bottom bg-gray-100">
                     <p class="project__duration text-dark-50 fw-normal fs-small mb-0">
-                      {{get_field('client_projects', $id)['duration']}}
+                      {{$clientProjects['duration']}}
                     </p>
 
-                    @if (get_field('client_projects', $id)['filter_tags'])
-                      @foreach (get_field('client_projects', $id)['filter_tags'] as $tag)
+                    @if ($clientProjects['filter_tags'])
+                      @foreach ($clientProjects['filter_tags'] as $tag)
                         <span class="badge bg-primary ms-2">{{$tag['tag']}}</span>
                       @endforeach
                     @endif
@@ -62,11 +62,11 @@
                     <h6 class="fw-bold mb-1 text-dark-75">About Them</h6>
                     <p class="mb-2 project__description">{{get_the_excerpt($id)}}</p>
 
-                    @if (get_field('client_projects', $id)['project_link'])
+                    @if ($clientProjects['project_link'])
                       <a
                         target="_blank"
                         class="btn btn-sm btn-dark px-3"
-                        href="{{get_field('client_projects', $id)['project_link']['url']}}"
+                        href="{{$clientProjects['project_link']['url']}}"
                       >
                         See Site Live
                       </a>
@@ -74,14 +74,14 @@
                   </div>
 
                   <div class="p-3 border-top">
-                    @if (get_field('client_projects', $id)['what_i_did'])
+                    @if ($clientProjects['what_i_did'])
                         <h6 class="fw-bold mb-1 text-dark-75">What I Did</h6>
-                        <p class="mb-2 project__description">{{get_field('client_projects', $id)['what_i_did']}}</p>
+                        <p class="mb-2 project__description">{{$clientProjects['what_i_did']}}</p>
                     @endif
 
                     <div class="project__goals">
                       <ul class="list-styled ms-4">
-                        @foreach (get_field('client_projects', $id)['goals'] as $goal)
+                        @foreach ($clientProjects['goals'] as $goal)
                           <li class="my-1 text-dark-75">{{$goal['goal']}}</li>
                         @endforeach
                       </ul>
@@ -93,9 +93,9 @@
             </div>
 
             <div class="project__gallery__container row mt-3 px-1">
-              @if (get_field('client_projects', $id)['gallery'])
-                @foreach (get_field('client_projects', $id)['gallery'] as $img)
-                  <div class="{{$img['col_width']}} position-relative mb-3">
+              @if ($clientProjects['gallery'])
+                @foreach ($clientProjects['gallery'] as $img)
+                  <div class="{{$img['col_width']}} position-relative mb-4">
                     <img
                       class="project__gallery__img rounded shadow w-100"
                       alt="{{$img['image_description']}}"
@@ -113,7 +113,7 @@
               @php echo get_post_field('post_content', $id) @endphp
             </section> --}}
           </div>
-        </article> 
+        </article>
       @endforeach
   </section>
 
