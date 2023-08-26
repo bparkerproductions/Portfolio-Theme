ce4f20a4ca30172315e65e7f08ab13f55f00c7ad
Brandon Parker <bparkerproductions@gmail.com>
2023-08-26 15:39:33 -0700

    improve button rotate

--- resources/views/blocks/bs-button/bs-button.php
+++ resources/views/blocks/bs-button/bs-button.php
@@ -1,13 +1,10 @@
-<a 
+<a
   href="<?= get_field('button_link')['url'] ?>"
   class="btn <?= get_field('classes') ?>"
 >
-  <span class="position-relative d-flex">
-    <?= get_field('button_link')['title']; ?>
-    <?php if (get_field('button_icon_class') ): ?>
-      <div class="btn__icon">
-        <i class="<?= get_field('button_icon_class') ?> ms-3 fa-lg"></i>
-      </div>
-    <?php endif; ?>
-  </span>
+  <?= get_field('button_link')['title']; ?>
+  
+  <?php if (get_field('button_icon_class') ): ?>
+    <i class="<?= get_field('button_icon_class') ?> ms-3 fa-lg"></i>
+  <?php endif; ?>
 </a>
--- resources/views/partials/components/button.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="button-container">
-  <a href="{{$link}}">
-    <button class="button {{$color}}">
-      {{$text}}
-      <i class="fa fa-caret-right"></i>
-    </button>
-  </a>
-</div>
