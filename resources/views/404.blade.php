f56ca4746a02316add72e3e38519b05081e90ccc
Brandon Parker <bparkerproductions@gmail.com>
2023-08-31 10:58:36 -0700

    404 page redesign

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,47 +1,44 @@
-@extends('layouts.app')
+@extends('layouts.minimal')
 
 @section('content')
-  @component('components.header', [
-    'icon_class' => false
-  ])
-    <h5 class="white">
-      <a href="{{get_home_url()}}">
-        <i class="fas fa-lg fa-home ml-small"></i> Go Home
-     </a>
-    </h5>
-  @endcomponent
-
-  <div id="not-found" class="column-center">
+  <section id="not-found" class="py-5 py-lg-7 header-scrolled">
     @if (!have_posts())
       <div class="container">
-        <h1 class="shade-black">404</h1>
-        <div class="icon-container">
-        <h3>Sorry, but the page you were trying to view does not exist.</h3>
-        <i class="fas fa-frown blue"></i>
-        </div>
+        <i class="fas fa-tired fa-4x mb-4 text-secondary"></i>
+        <h1 class="text-dark">404 Page Not Found... Looks like you are lost</h1>
 
-        <div class="link-container">
+        <div class="mt-4 card card-body col-12 col-lg-6">
 
-          <h6>Would you like to</h6>
-          <ul>
-            <li>
-              <a class="link black" href="{{get_home_url()}}">
-                <i class="fas fa-home blue"></i> Go To Home Page?
+          <h6 class="text-dark fw-bold">Would you like to</h6>
+          <ul class="list-unstyled ms-3">
+            <li class="my-1">
+              <a class="text-dark text-decoration-underline" href="{{get_home_url()}}">
+                Go back to home?
               </a>
             </li>
-            <li>
-              <a class="link black" href="{{$blog_link}}">
-                <i class="fas fa-newspaper blue"></i> Go To Blog Posts?
+            <li class="my-1">
+              <a class="text-dark text-decoration-underline" href="{{$blog_link}}">
+                View blog posts?
               </a>
             </li>
-            <li>
-              <a class="link black" href="{{$projects_link}}">
-                <i class="fas fa-laptop-code blue"></i> Go To Personal Projects?
+            <li class="my-1">
+              <a class="text-dark text-decoration-underline" href="{{$projects_link}}">
+                View all of my projects
+              </a>
+            </li>
+            <li class="my-1">
+              <a class="text-dark text-decoration-underline" href="{{$portfolio_link}}">
+                View my portfolio
               </a>
             </li>
           </ul>
+
+          <div class="bg-gray-200 border rounded p-3 position-relative">
+            <a href="{{$resume_link}}" class="absolute-fill"></a>
+            <p class="mb-0"><i class="fas fa-file-alt me-2"></i>View Resume</p>
+          </div>
         </div>
       </div>
     @endif
-  </div>
+    </section>
 @endsection
---
+++ resources/views/layouts/minimal.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html {!! get_language_attributes() !!}>
+  @include('partials.head')
+  <body @php body_class() @endphp>
+    @php do_action('get_header') @endphp
+    <div class="content">
+      @include('partials.global.navbar', [
+        'class' => $navbarClass ?? ''
+      ])
+      <main class="main">
+        @yield('content')
+      </main>
+    </div>
+    @php do_action('get_footer') @endphp
+    @php wp_footer() @endphp
+  </body>
+</html>
