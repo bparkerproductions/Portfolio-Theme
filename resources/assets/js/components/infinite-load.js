a6fb182c250c7990f5fdee44ec798486c6a852fb
Brandon Parker <bparkerproductions@gmail.com>
2023-08-31 19:44:30 -0700

    infinite-load spinner logic and design

--- resources/assets/js/components/infinite-load.js
+++ resources/assets/js/components/infinite-load.js
@@ -2,8 +2,39 @@ import Helpers from './../helpers/general.js'
 
 (function() {
   window.addEventListener('DOMContentLoaded', () => {
-    if ( !Helpers.hasElement('.infinite-load') ) return
+    if ( !Helpers.hasElement('.infinite-load') ) return;
 
-    console.log('infinite-load');
+    const parent = document.querySelector('.infinite-load');
+
+    function createSpinner() {
+      const spinnerContainer = document.createElement('div');
+      spinnerContainer.classList.add('spinner', 'infinite-load');
+
+      const spinner = document.createElement('i');
+      spinner.classList.add('fas', 'fa-circle-notch', 'fa-4x');
+      spinnerContainer.append(spinner);
+
+      const $spinnerEl = document.querySelector('.spinner.infinite-load');
+
+      if ( !$spinnerEl ) {
+        parent.append(spinnerContainer);
+      }
+    }
+
+    function fetchPosts() {
+      createSpinner();
+
+      // Fetch posts, etc
+    }
+
+    function onScroll() {
+      const targetEl = parent.getBoundingClientRect();
+
+      if ( targetEl.bottom <= window.innerHeight ) {
+        fetchPosts();
+      }
+    }
+
+    document.addEventListener( 'scroll', onScroll );
   })
 })()
