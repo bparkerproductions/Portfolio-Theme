2fa0e4c38fcaacd713d6b472fd39aad71943ea24
Brandon Parker <bparkerproductions@gmail.com>
2023-09-01 17:04:14 -0700

    blog categorie slight redesign

--- resources/views/partials/blog-categories.blade.php
+++ resources/views/partials/blog-categories.blade.php
@@ -1,8 +1,9 @@
 <section class="pt-4 pb-5 container blog-categories">
-    <div class="d-flex flex-wrap flex-xl-nowrap">
+    <div class="d-flex flex-wrap">
       <a
-        class="{{!single_cat_title('', false) ? 'selected' : ''}} blog-categories__category me-2 me-xl-4
-        badge btn btn-primary px-3 py-2 rounded-5 my-2 my-xl-0"
+        class="{{!single_cat_title('', false) ? 'selected' : ''}}
+        blog-categories__category text-primary fs-sm me-4 fs-6 my-2"
+        title="Go to the main blog page"
         href="{{$blog_link}}"
       >
         All Categories
@@ -11,11 +12,12 @@
         @if($cat->count >= 3)
           <a
             href="{{get_category_link($cat->cat_ID)}}"
+            title="Go to the {{$cat->name}} category page"
             class="{{Archive::isSelected($cat)}} blog-category__category me-2
-             badge btn btn-secondary px-3 py-2 rounded-5 my-2 my-xl-0"
+             badge bg-secondary px-3 text-white rounded-5 d-flex align-items-center my-2 py-2"
           >
-            {{$cat->name}} <span class="blog-categories__count badge bg-white rounded-5 text-dark ms-2">
-              {{$cat->count}}
+            {{$cat->name}} <span class="blog-categories__count ms-2 fs-xs">
+              ({{$cat->count}})
             </span>
           </a>
         @endif
