0882fcebcd47e33f8352b65234ccfcbb83c3e561
Brandon Parker <bparkerproductions@gmail.com>
2023-09-02 10:30:15 -0700

    search bar component initial design

--- resources/views/partials/searchbar.blade.php
+++ resources/views/partials/searchbar.blade.php
@@ -1,7 +1,28 @@
-<div class="col-12 col-lg-6 mb-4">
+<div class="search-bar px-0 col-12 col-lg-6 mb-4 position-relative">
   <input
     type="text"
     placeholder="Search Content..."
     class="input-style input-lg w-100">
   </input>
+
+  <div class="search-bar__results">
+    <ul class="search-bar__list list-unstyled card">
+      <li>
+        <i class="fas fa-file-alt me-3"></i>
+        A Common Guide to Algorithms
+      </li>
+      <li>
+        <i class="fas fa-file-alt me-3"></i>
+        A Common Guide to Algorithms
+      </li>
+      <li>
+        <i class="fas fa-file-alt me-3"></i>
+        A Common Guide to Algorithms
+      </li>
+      <li>
+        <i class="fas fa-file-alt me-3"></i>
+        A Common Guide to Algorithms
+      </li>
+    </ul>
+  </div>
 </div>
