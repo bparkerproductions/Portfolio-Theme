5c5069f9925a120e503193312a3f4d328e86eeef
Brandon Parker <bparkerproductions@gmail.com>
2023-09-10 11:04:32 -0700

    block title and markdown support

--- resources/views/blocks/bs-alert/bs-alert.php
+++ resources/views/blocks/bs-alert/bs-alert.php
@@ -1,7 +1,13 @@
-<div class="alert alert-<?= get_field('alert_color') ?> d-flex flex-column flex-lg-row align-items-start">
-  <?php if(get_field('alert_icon_class')): ?>
-    <i class="<?= get_field('alert_icon_class') ?> mt-0 mt-lg-2 me-0 me-lg-3 mb-2 mb-lg-0"></i>
+<div class="block__alert alert alert-<?= get_field('alert_color') ?>">
+  <?php if (get_field('alert_title')): ?>
+    <h6 class="text-dark mb-3"><?= get_field('alert_title') ?></h6>
   <?php endif; ?>
 
-  <p class="mb-0"><?= get_field('alert_text'); ?></p>
+  <div class="d-flex align-items-start">
+    <?php if(get_field('alert_icon_class')): ?>
+      <i class="<?= get_field('alert_icon_class') ?> mt-2 me-3"></i>
+    <?php endif; ?>
+
+    <div class="alert-content"><?= get_field('alert_text'); ?></div>
+  </div>
 </div>
