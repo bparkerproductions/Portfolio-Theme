0d59a7eed33dba76743f5850af99ce49e48244c3
Brandon Parker <bparkerproductions@gmail.com>
2023-12-02 12:07:43 -0800

    add corner icon element and degree section on portfolio

--- resources/views/partials/credentials.blade.php
+++ resources/views/partials/credentials.blade.php
@@ -1,39 +1,45 @@
 <ul class="list-unstyled row">
+
+  {{-- Degree --}}
+  <li class="col-12 mb-2">
+    @include('partials.degree')
+  </li>
+
   @foreach( get_field('credentials', 'option') as $credential )
     <li class="col-12 col-lg-6 my-2 d-flex align-items-center credential position-relative">
-      <div class="d-flex flex-column bg-gray-400 p-3 w-100 h-100 animation--hover-link border">
+      <div class="d-flex flex-column bg-gray-400 p-3 w-100 h-100">
         <h6 class="d-block d-sm-none">{{$credential['title']}}</h6>
 
         <div class="d-flex">
-        <div class="d-flex align-items-center me-3 py-2">
-          <img
-            class="credential__image"
-            src="{{$credential['image']['url']}}"
-            alt="An Image of the {{$credential['title']}} badge"
-          />
-        </div>
+          <div class="d-flex align-items-center me-3 py-2">
+            <img
+              class="credential__image"
+              src="{{$credential['image']['url']}}"
+              alt="An Image of the {{$credential['title']}} badge"
+            />
+          </div>
 
-        <div class="d-flex flex-column justify-content-center">
-          <h6 class="d-none d-sm-block">{{$credential['title']}}</h6>
-          @if ($credential['link'])
-            <a
-              target="_blank"
-              href="{{$credential['link']['url']}}"
-              class="absolute-fill"
-            ></a>
+          <div class="d-flex flex-column justify-content-center">
+            <h6 class="d-none d-sm-block">{{$credential['title']}}</h6>
+            @if ($credential['link'])
+              <a
+                target="_blank"
+                href="{{$credential['link']['url']}}"
+                class="absolute-fill"
+              ></a>
 
-            <p class="mb-0 text-dark text-decoration-underline">
-              {{$credential['link']['title']}}
-              <i class="fas fa-external-link-alt fa-sm ms-2"></i>
-            </p>
-          @endif
+              <p class="mb-0 text-dark text-decoration-underline">
+                {{$credential['link']['title']}}
+                <i class="fas fa-external-link-alt fa-sm ms-2"></i>
+              </p>
+            @endif
 
-          @if ( $credential['certificate_number'] )
-            <p class="text-dark-50 fw-light mb-0 fs-sm">{{$credential['certificate_number']}}</p>
-          @endif
+            @if ( $credential['certificate_number'] )
+              <p class="text-dark-50 fw-light mb-0 fs-sm">{{$credential['certificate_number']}}</p>
+            @endif
 
-          <p class="mb-0 mt-1 text-dark-50">Issued on {{$credential['date_issued']}}</p>
-        </div>
+            <p class="mb-0 mt-1 text-dark-50">Issued on {{$credential['date_issued']}}</p>
+          </div>
         </div>
       </div>
     </li>
---
+++ resources/views/partials/degree.blade.php
@@ -0,0 +1,15 @@
+<div class="bg-gray-400 p-3 w-100 h-100 position-relative">
+  <div class="corner-icon bg-dark">
+    <i class="corner-icon__icon fas fa-graduation-cap fa-lg text-white"></i>
+  </div>
+
+  <div class="ms-5">
+    <p class="fw-bold fs-5 mb-2">Bachelor of Science in Software Engineering</p>
+    <a
+      class="fw-light text-decoration-underline text-dark-75 mb-1"
+      _target="blank"
+      href="https://www.wgu.edu/online-it-degrees.html"
+    >Western Governer's University</a>
+    <p class="fw-semibold text-dark-75 mb-0">Graduated December 2023</p>
+  </div>
+</div>
